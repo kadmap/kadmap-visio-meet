54fdeb42b8bb9596c58df5cb03748761b363fe0c
ojichinedu <ojichinedu4@gmail.com>
2025-05-15 08:09:31 +0100

    changes

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), tsconfigPaths()],
     server: {
-      port: parseInt(env.VITE_PORT) || 3000,
+      port: parseInt(env.VITE_PORT) || 3007,
       host: env.VITE_HOST || 'localhost',
       allowedHosts: ['.nip.io'],
     },
